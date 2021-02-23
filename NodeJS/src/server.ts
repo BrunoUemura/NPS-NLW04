6ba1ca4f9c14d916d90c792abb0fc30bdbd2ef3b
BrunoUemura <bruno.h.uemura@outlook.com>
2021-02-23 20:31:00 -0300

    NLW04 Projects

---
+++ NodeJS/src/server.ts
@@ -0,0 +1,21 @@
+import "reflect-metadata";
+import express, { response } from "express";
+import "./database";
+import { router } from "./routes";
+
+const app = express();
+
+/**
+ * GET => Busca
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração específica
+ */
+
+app.use(express.json());
+app.use(router);
+
+app.listen(3333, () => {
+  console.log("Server is running");
+});
