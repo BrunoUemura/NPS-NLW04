9b576730132bf7a939d69d9ee0626b43545e8b7e
BrunoUemura <bruno.h.uemura@outlook.com>
2021-02-25 22:28:54 -0300

    class 04 - NodeJS

--- NodeJS/src/routes.ts
+++ NodeJS/src/routes.ts
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import { UserController } from "./controllers/UserController";
 import { SurveysController } from "./controllers/SurveysController";
+import { SendMailController } from "./controllers/SendMailController";
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 
+const sendMailController = new SendMailController();
+
 router.get("/users", userController.show);
 router.post("/users", userController.create);
 
 router.get("/surveys", surveysController.show);
 router.post("/surveys", surveysController.create);
+
+router.post("/sendmail", sendMailController.execute);
 
 export { router };
